f67255c21e3b123f9e9c8c81110976db930e8a01
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-07-10 13:34:51 -0500

    fullstackopen: part2  phonebook, exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((resp) => {
+      const persons = resp.data;
+      setPersons(persons);
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -50,7 +53,6 @@ const App = () => {
 
       <h2>Numbers</h2>
       <Persons filter={filter} persons={persons} />
-
     </div>
   );
 };
