6212ddc6b49f66d7a4b3b3e250dab2992953b8f3
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-08-04 22:14:38 -0500

    5.3: bloglist frontend, step3

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,41 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import authService from './services/auth';
-import blogService from './services/blogs';
+import * as  blogService from './services/blogs';
+
+const initialBlogForm = {
+  title: '',
+  author: '',
+  url: '',
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [blog, setBlog] = useState(initialBlogForm);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  useEffect( () => {
-    const userStorage = localStorage.getItem('user')
+  useEffect(() => {
+    const userStorage = localStorage.getItem('user');
     if (userStorage) {
-      const userData = JSON.parse(userStorage)
-      setUser(userData)
+      const userData = JSON.parse(userStorage);
+      setUser(userData);
     }
-  }, [])
+  }, []);
+
 
   const handleLogin = (e) => {
     e.preventDefault();
     const credentials = { username, password };
 
-    authService.login(credentials).then(user => {
-      setUser(user)
-      const parsedUser = JSON.stringify(user)
-      localStorage.setItem('user',parsedUser)
+    authService.login(credentials).then((user) => {
+      setUser(user);
+      const parsedUser = JSON.stringify(user);
+      localStorage.setItem('user', parsedUser);
     });
   };
 
-  const handleLogout = e => {
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  const handleBlogInput = e => {
+    const { value, name } = e.target
+    setBlog(prev => ({
+      ...prev,
+      [name]: value
+    }))
+  }
+
+  const handleNewNote = e => {
     e.preventDefault()
-    localStorage.removeItem('user')
-    setUser(null)
+    blogService.createNew(blog, user.token)
+      .then(resp => {
+        const {user, ...newBlog} = resp
+        setBlog(initialBlogForm)
+        setBlogs(blogs => blogs.concat(newBlog))
+      })
   }
 
   if (!user) {
@@ -72,6 +98,39 @@ const App = () => {
         {user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+
+      <h2>create new</h2>
+      <form onSubmit={handleNewNote}>
+        <div>
+          <label>title</label>
+          <input
+            type="text"
+            name="title"
+            value={blog.title}
+            onChange={handleBlogInput}
+          />
+        </div>
+        <div>
+          <label>author</label>
+          <input
+            type="text"
+            name="author"
+            value={blog.author}
+            onChange={handleBlogInput}
+          />
+        </div>
+        <div>
+          <label>url</label>
+          <input
+            type="text"
+            name="url"
+            value={blog.url}
+            onChange={handleBlogInput}
+          />
+        </div>
+        <button>create</button>
+      </form>
+
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -1,11 +1,13 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+export const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default {
-  getAll
-}
+export const createNew = (blog, token) => {
+  const headers = { authorization: `Bearer ${token}` };
+  const request = axios.post(baseUrl, blog, { headers });
+  return request.then((resp) => resp.data);
+};
