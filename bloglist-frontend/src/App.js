5f4adfddbc9265188ac3ef623e1e946c36a0e0f8
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-08-04 21:06:27 -0500

    5.2: bloglist frontend, step2

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -13,22 +13,36 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  const handleSubmit = (e) => {
+  useEffect( () => {
+    const userStorage = localStorage.getItem('user')
+    if (userStorage) {
+      const userData = JSON.parse(userStorage)
+      setUser(userData)
+    }
+  }, [])
+
+  const handleLogin = (e) => {
     e.preventDefault();
     const credentials = { username, password };
 
     authService.login(credentials).then(user => {
       setUser(user)
       const parsedUser = JSON.stringify(user)
-      localStorage.setItem('')
+      localStorage.setItem('user',parsedUser)
     });
   };
+
+  const handleLogout = e => {
+    e.preventDefault()
+    localStorage.removeItem('user')
+    setUser(null)
+  }
 
   if (!user) {
     return (
       <div>
         <h2>Log in to Aplication</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleLogin}>
           <div>
             <label>username</label>
             <input
@@ -54,7 +68,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p> {user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
