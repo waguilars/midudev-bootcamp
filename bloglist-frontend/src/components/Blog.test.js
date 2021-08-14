7da67e771640bcc2c663bf13d047591c11ada3ed
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-08-13 20:24:03 -0500

    5.14*: Blog list tests, step2, test that check the url and likes are displayed when show button is clicked

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 
 import Blog from './Blog'
 
-test('should render displaying blog\'s title and author', () => {
-  const blog = {
-    title: 'Structure based in components',
-    author: 'Wilson Aguilar',
-    url: 'https://...',
-    likes: 24,
-    user: {
-      username: 'will',
-      name: 'Wilson',
-      id: '610859a4ededbefa616a658f',
-    },
-    id: '610859f3bb7707fb27458d41',
-  }
+const showButtonLabel = 'show'
+const blog = {
+  title: 'Structure based in components',
+  author: 'Wilson Aguilar',
+  url: 'https://...',
+  likes: 24,
+  user: {
+    username: 'will',
+    name: 'Wilson',
+    id: '610859a4ededbefa616a658f',
+  },
+  id: '610859f3bb7707fb27458d41',
+}
 
-  const component = render(<Blog blog={blog} />)
+
+test('should render displaying blog\'s title and author', () => {
+  const  component = render(<Blog blog={blog} />)
 
   expect(component.container).toHaveTextContent(blog.author)
   expect(component.container).toHaveTextContent(blog.title)
   expect(component.container).not.toHaveTextContent(blog.url)
   expect(component.container).not.toHaveTextContent(blog.likes)
 
+})
 
+test('should render url and likes when show button is clicked', () => {
+  const  component = render(<Blog blog={blog} authUser={blog.user} />)
+  const button = component.getByText(showButtonLabel)
+  fireEvent.click(button)
 
+  expect(component.container).toHaveTextContent(blog.likes)
+  expect(component.container).toHaveTextContent(blog.url)
+  // console.log(prettyDOM(component.container))
 })
