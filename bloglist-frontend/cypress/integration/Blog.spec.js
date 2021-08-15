c1c7767bfaccb8b6f67a4fa8fa183473205a8002
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-08-15 14:44:26 -0500

    5.18: bloglist end to end testing, step2 - e2e test login wrong and succeeds

--- bloglist-frontend/cypress/integration/Blog.spec.js
+++ bloglist-frontend/cypress/integration/Blog.spec.js
@@ -1,13 +1,39 @@
 
 /// <reference types="cypress" />
 
+const user = { username: 'will', password: 'password', name: 'Will Testing' }
+
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    cy.request('POST', 'http://localhost:3003/api/users' , user)
+
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.get('#login-form')
   })
+
+
+  describe('Login',function() {
+
+    it('fails with wrong credentials', function() {
+      cy.get('[name="username"]').type('another user')
+      cy.get('[name="password"]').type('another password')
+
+      cy.get('#login-form').submit()
+      cy.contains('invalid').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
+
+    it('succeeds with correct credentials', function() {
+      cy.get('[name="username"]').type(user.username)
+      cy.get('[name="password"]').type(user.password)
+
+      cy.get('#login-form').submit()
+    })
+  })
+
 })
