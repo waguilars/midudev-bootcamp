118e462a6a1394466ec44dcf9d0b36d189a5253b
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-08-28 20:49:51 -0500

    :sparkles: 6.19, step 1 - Use connect instead hooks inside notification.

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,9 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = ({notification}) => {
+  // const notification = useSelector(state => state.notification)
 
   const style = {
     border: 'solid',
@@ -17,4 +17,12 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = state => {
+  return {
+    notification: state.notification
+  }
+}
+
+const connectedNotification = connect(mapStateToProps)(Notification)
+
+export default connectedNotification
